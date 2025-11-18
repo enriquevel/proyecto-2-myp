12cbbfcff24df56d2905d6acc8fa97c71281f4ad
enriquevel <enriquev@ciencias.unam.mx>
2025-11-18 16:07:59 -0600

    documentacion de scorer

--- src/main/java/myp/proyecto2/model/scorer/SafetyScorer.java
+++ src/main/java/myp/proyecto2/model/scorer/SafetyScorer.java
@@ -5,15 +5,25 @@ import java.util.EnumMap;
 import myp.proyecto2.model.domain.Report;
 import myp.proyecto2.model.domain.ReportType;
 
+/**
+ * Calificador de rutas enfocadas en seguridad, que priorizan este rubro.
+ */
 public class SafetyScorer extends RouteScorer {
 
+    /** Mapa de pesos personalizados para cada tipo de reporte.*/
     private final Map<ReportType, Double> weights;
 
+    /**
+     * Constructor que inicializa los pesos especificos para seguridad.
+     */
     public SafetyScorer() {
         this.weights = new EnumMap<>(ReportType.class);
         defineWeights();
     }
 
+    /**
+     * Metodo auxiliar que define el valor de los pesos, con un enfoque en seguridad.
+     */
     private void defineWeights() {
         weights.put(ReportType.CRIME_INCIDENT, 5.0);
         weights.put(ReportType.ACCIDENT, 4.0);          
@@ -26,11 +36,22 @@ public class SafetyScorer extends RouteScorer {
         weights.put(ReportType.OTHER, 1.0);
     }
 
+    /**
+     * Devuelve el nombre del calificador de rutas.
+     * 
+     * @return el nombre del calificador de rutas.
+     */
     @Override
     public String getName() {
         return "Safety scorer";
     }
 
+    /**
+     * Calcula la penalizacion para un reporte especifico, tomando en cuenta los pesos de seguridad.
+     * 
+     * @param report el reporte a evaluar.
+     * @return la penalizacion en segundos que el reporte aporta al score de la ruta.
+     */
     @Override
     public double calculatePenalty(Report report) {
         if (report == null || !report.isActive()) {
